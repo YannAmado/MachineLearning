e554ac46bf28d87e14dc41f219b9dc025dd2e2a4
YannAmado <yannamado.n@gmail.com>
2022-10-22 23:33:36 +0200

    some bugfixes

--- Tutorials/MNIST_debugger.py
+++ Tutorials/MNIST_debugger.py
@@ -2,9 +2,9 @@ import numpy as np
 import random as rd
 import mnist
 
-
 class Network:
     def __init__(self, layers: list):
+        np.random.seed(42)
         b = []
         w = []
         a = []
@@ -34,12 +34,11 @@ def sigmoid_derivative(n: float):
     return sigmoid(n)*(1-sigmoid(n))
 
 
-
 def feedForward(net: Network) -> Network:
     for l in range(0, net.nLayers - 1):
         for receivingNeuron in range(net.layers[l + 1]):
             # resetting the z as to not take any info from the activation of the previous number
-            net.z[l + 1][receivingNeuron] = 0
+            net.z[l+1][receivingNeuron] = 0
             for givingNeuron in range(net.layers[l]):
                 net.z[l + 1][receivingNeuron] += net.a[l][givingNeuron] * net.w[l][givingNeuron][receivingNeuron]
             net.z[l + 1][receivingNeuron] += net.b[l][receivingNeuron]
@@ -51,6 +50,7 @@ def feedForward(net: Network) -> Network:
 def setInput(net: Network, MNISTnumber):
     numberArr = np.asarray(MNISTnumber).flatten()
     for i in range(net.layers[0]):
+        net.z[0][i] = numberArr[i]
         net.a[0][i] = numberArr[i]
     net = feedForward(net)
 
@@ -82,17 +82,18 @@ def backProp(net: Network, delta, batchSize, learningRate) -> Network:
 
 def SGD(net: Network, X: list, y: list, batchSize: int, nEpochs: int, learningRate) -> Network:
     for epoch in range(nEpochs):
-        print(epoch)
-        delta = 0
+        # print(epoch)
+        delta = np.zeros(10)  # 10 because its the possible number of outputs
         batch = rd.sample(range(len(X)), batchSize)
         for i in batch:
             net = setInput(net, X[i])
             # not too sure about the meaning of the y in the equation (a^L_j - y_j)
+            # not sure about how the delta should be calculated
             for j in range(net.layers[-1]):
                 if y[i] == j:
-                    delta += (net.a[-1][j] - 1) * sigmoid_derivative(net.z[-1])
+                    delta[j] += (net.a[-1][j] - 1) * sigmoid_derivative(net.z[-1][j])
                 else:
-                    delta += (net.a[-1][j] - 0) * sigmoid_derivative(net.z[-1])
+                    delta[j] += (net.a[-1][j] - 0) * sigmoid_derivative(net.z[-1][j])
 
         # taking the average of the results
         delta = delta / batchSize
@@ -100,8 +101,35 @@ def SGD(net: Network, X: list, y: list, batchSize: int, nEpochs: int, learningRa
     return net
 
 
+def testNetwork(net: Network, test_X, test_y, batchSize: int):
+    correctOutput = 0
+    X = test_X[:batchSize]
+    y = test_y[:batchSize]
+    outputs = np.zeros(10)
+    for i in range(batchSize):
+        net = setInput(net, X[i])
+        networkOutput = np.argmax(net.a[-1])
+        outputs[networkOutput] += 1
+        #print(f"number: {y[i]}, networkOutput: {networkOutput}, activations: {net.a[-1]}")
+        if y[i] == networkOutput:
+            correctOutput += 1
+    acc = correctOutput/batchSize
+    return acc, outputs
+
+def gridSearch(net: Network, train_X, train_y, test_X, test_y, batchSize: int, learningRates: list, epochs: list):
+    for eta in learningRates:
+        for epoch in epochs:
+            # resetting the network
+            net = Network([784,30,10])
+            net = SGD(net, train_X, train_y, batchSize=batchSize, nEpochs=epoch, learningRate=eta)
+            acc, outputs = testNetwork(net, test_X, test_y, batchSize=batchSize)
+            print(f'learningRate: {eta} epochs: {epoch} acc: {acc}, outputs: {outputs}')
+
+
 net = Network([784,30,10])
 train_X = mnist.train_images()
 train_y = mnist.train_labels()
+test_X = mnist.test_images()
+test_y = mnist.test_lavels()
 
-net = SGD(net, train_X, train_y, 100, 50, 0.05)
+gridSearch(net, train_X, train_y, test_X, test_y, batchSize=100, learningRates=[0.1,1,10,100,1000,10000], epochs=[10,20,50])
